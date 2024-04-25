4f79ce76f1976795b01389ca8c953dff786f61fb
Oleksii <alextrenal13@gmail.com>
2024-04-25 13:37:57 +0200

    Leetcode task was done

---
+++ src/Leetcode.java
@@ -0,0 +1,45 @@
+public class Leetcode {
+
+
+    //    Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    class Solution {
+        ListNode head;
+
+        public ListNode reverseList(ListNode head) {
+            ListNode prevNode = null;
+            ListNode currentNode = head;
+            ListNode nextNode = null;
+
+            while (currentNode != null) {
+                nextNode = currentNode.next;
+                currentNode.next = prevNode;
+                prevNode = currentNode;
+                currentNode = nextNode;
+            }
+            return prevNode;
+        }
+
+        public Solution(ListNode head) {
+            this.head = head;
+        }
+    }
+
+}
