bf631cf1a1faf7b080fd9c755e93ebe70eb30ed3
Oleksii <alextrenal13@gmail.com>
2024-05-16 15:43:24 +0200

    Home task 10 was done.

--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,19 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 
 public class Main {
     public static void main(String[] args) {
-        String[] words = new String[14];
-        Arrays.fill(words, "apple");
-        System.out.println("Occurance " + countOccurance(words, "apple"));
-        String[] arr = {"a", "b"};
-        List<String> afterToList = toList(arr);
-        System.out.println(afterToList);
-        ArrayList<Integer> integers = new ArrayList<>();
-        integers.addAll(Arrays.asList(3, 4, 5, 2, 4, 66, 3, 23, 6, 4));
-        System.out.println("Only unique ints: " + findUnique(integers));
-        List<String> words2 = List.of("apple", "bird", "fox", "fox", "apple", "apple", "bird", "bear", "bear", "cat", "frog", "frog", "frog", "frog", "frog");
-        calcOccurance(words2);
-        System.out.println(findOccurance(words2));
-    }
-
-
-    public static int countOccurance(String[] words, String target) {
-        int occurance = 0;
-        for (String word : words) {
-            if (word.equals(target)) {
-                occurance++;
+        String[][] matrix = new String[4][4];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = String.valueOf(i + j);
             }
         }
-        return occurance;
-    }
-
-    public static <T> List<T> toList(T[] toConvert) {
-        List<T> converted = new ArrayList<>();
-        Collections.addAll(converted, toConvert);
-        return converted;
-    }
-
-    public static List<Integer> findUnique(ArrayList<Integer> integers) {
-        return new ArrayList<>(new HashSet<>(integers));
-    }
-
-    public static void calcOccurance(List<String> words) {
-        Set<String> unique = new HashSet<>(words);
-
-        for (String word : unique) {
-            int currentOccurance = 0;
-            for (String word2 : words) {
-                if (word.equals(word2)) {
-                    currentOccurance++;
-                }
-            }
-            System.out.println("\"" + word + "\"" + " occurance: " + currentOccurance);
+//        matrix[1][1] = "Hello";
+        ArrayValueCalculator calc = new ArrayValueCalculator();
+        try {
+            System.out.println("Sum of the matrix: " + calc.doCalac(matrix));
+        } catch (ArraySizeException | ArrayDataException e) {
+            System.out.println(e);
         }
-    }
 
-    public static List<OccuranceItem> findOccurance(List<String> words) {
-        Set<String> unique = new HashSet<>(words);
-        List<OccuranceItem> occuranceItems = new ArrayList<>();
-        for (String word : unique) {
-            int currentOccurance = 0;
-            for (String word2 : words) {
-                if (word.equals(word2)) {
-                    currentOccurance++;
-                }
-            }
-            occuranceItems.add(new OccuranceItem(currentOccurance, word));
-        }
-        return occuranceItems;
     }
 }
